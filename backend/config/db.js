9246c5a30744610cd4b1494eb91d3d07a3599398
samri2 <wsamrawit2@gmail.com>
2025-11-19 14:23:40 +0400

    render

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // important for Render
+    },
+  },
+});
 
 export default sequelize;
