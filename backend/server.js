2b2e24b10cf17ce126a5fde7fb8dc156544fcbde
samri2 <wsamrawit2@gmail.com>
2025-11-19 14:39:11 +0400

    render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get('/', (req, res) => {
+  res.send('Your backend API is running!'); // Or res.json({ status: 'ok' })
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/documents", docRoutes);
