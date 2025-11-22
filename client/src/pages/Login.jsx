fa26353212daec3c74c6158af01127f15c05dc31
samri2 <wsamrawit2@gmail.com>
2025-11-22 11:13:03 +0400

    NEW

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,8 +25,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch(`
-https://document-sharing-platform-3.onrender.com/api/v1/auth/login`, {
+      const res = await fetch(`${API_BASE}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
